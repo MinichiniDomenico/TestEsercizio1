60d7e68b36eb44d923ff76fd6426cb883d878322
MinichiniDomenico <142994930+MinichiniDomenico@users.noreply.github.com>
2023-08-24 13:22:00 +0000

    Correttiva route

--- src/Backend/React/Extension/RouteRegistrationExtensions.cs
+++ src/Backend/React/Extension/RouteRegistrationExtensions.cs
@@ -9,8 +9,8 @@ static class RouteRegistrationExtensions
     {
         var apiGroup = app.MapGroup("api");
 
-        apiGroup.MapGet("suppliers/list", async ([AsParameters]SupplierListQuery query, IMediator mediator) => await mediator.Send(query)).WithName("GetSuppliersList").WithOpenApi();
-        apiGroup.MapGet("employees/list", async ([AsParameters]EmployeesListQuery query, IMediator mediator) => await mediator.Send(query)).WithName("GetEmployeesList").WithOpenApi();
+        apiGroup.MapGet("suppliers/get/list", async ([AsParameters]SupplierListQuery query, IMediator mediator) => await mediator.Send(query)).WithName("GetSuppliersList").WithOpenApi();
+        apiGroup.MapGet("employees/get/list", async ([AsParameters]EmployeesListQuery query, IMediator mediator) => await mediator.Send(query)).WithName("GetEmployeesList").WithOpenApi();
         apiGroup.MapGet("customer/get/list", async ([AsParameters] CustomerListQuery query, IMediator mediator) => await mediator.Send(query)).WithName("GetCustomersList").WithOpenApi();
         apiGroup.MapGet("customer/get/xmlList", async ([AsParameters] CustomerListXmlQuery query, IMediator mediator) => await mediator.Send(query)).WithName("GetCustomerListXmlExtract").WithOpenApi();
     }
